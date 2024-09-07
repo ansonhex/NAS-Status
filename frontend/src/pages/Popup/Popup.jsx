77af771e59ca9360398170fb4d3c03cf75a18aa4
ansonhex <ansonhe1997@gmail.com>
2024-09-06 20:39:38 -0700

    added frontend

---
+++ frontend/src/pages/Popup/Popup.jsx
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  Heading,
+  Text,
+  Spinner,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  CircularProgress,
+  CircularProgressLabel,
+  VStack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+const getColorScheme = (percentage) => {
+  if (percentage < 50) return "green";
+  if (percentage < 70) return "yellow";
+  return "red";
+};
+
+const getTemperatureColor = (temperature) => {
+  if (temperature < 70) return "green";
+  if (temperature < 80) return "yellow";
+  return "red";
+};
+
+const Popup = () => {
+  const [data, setData] = useState({ cpu: null, ram: null, docker: [] });
+  const [loading, setLoading] = useState(false);
+  const bgColor = useColorModeValue("gray.100", "gray.800");
+  const textColor = useColorModeValue("gray.800", "white");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const [cpuResponse, ramResponse, dockerResponse] = await Promise.all([
+          fetch('http://192.168.1.243:3000/status/cpu'),
+          fetch('http://192.168.1.243:3000/status/ram'),
+          fetch('http://192.168.1.243:3000/status/docker'),
+        ]);
+
+        const [cpu, ram, docker] = await Promise.all([
+          cpuResponse.json(),
+          ramResponse.json(),
+          dockerResponse.json(),
+        ]);
+
+        setData({ cpu, ram, docker });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  return (
+    <Box bg={bgColor} color={textColor} minH="80vh" p={2}>
+      <Heading size="sm" mb={2} textAlign="center">
+        NAS Status Monitor
+      </Heading>
+
+      <Tabs isFitted variant="soft-rounded" colorScheme="purple">
+        <TabList mb="1em">
+          <Tab fontSize={"sm"} >CPU</Tab>
+          <Tab fontSize={"sm"} >RAM</Tab>
+          <Tab fontSize={"sm"} >Docker</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <VStack spacing={2} align="center" minH="100px">
+              {data.cpu ? (
+                <>
+                  <CircularProgress
+                    value={data.cpu.averageUsagePercentage}
+                    color={`${getColorScheme(data.cpu.averageUsagePercentage)}.400`}
+                    size="60px"
+                    thickness="12px"
+                    capIsRound
+                  >
+                    <CircularProgressLabel fontSize="xs" fontWeight={"bold"}>
+                      {data.cpu.averageUsagePercentage.toFixed(2)}%
+                    </CircularProgressLabel>
+                  </CircularProgress>
+                  <Text fontWeight="bold" textAlign="center">
+                    {data.cpu.model}
+                  </Text>
+                  <Text>Cores: {data.cpu.cores}</Text>
+                  <Text>Temperature: {data.cpu.temperature || "N/A"}°C</Text>
+                </>
+              ) : loading ? (
+                <Spinner />
+              ) : (
+                <Text>No CPU data available</Text>
+              )}
+            </VStack>
+          </TabPanel>
+          <TabPanel>
+            <VStack spacing={2} align="center" minH="100px">
+              {data.ram ? (
+                <>
+                  <CircularProgress
+                    value={data.ram.usagePercentage}
+                    color={`${getColorScheme(data.ram.usagePercentage)}.400`}
+                    size="60px"
+                    thickness="12px"
+                    capIsRound
+                  >
+                    <CircularProgressLabel fontSize="xs" fontWeight={"bold"}>
+                      {data.ram.usagePercentage.toFixed(2)}%
+                    </CircularProgressLabel>
+                  </CircularProgress>
+                  <Text>Total Memory: {data.ram.totalMemory}</Text>
+                  <Text>Free Memory: {data.ram.freeMemory}</Text>
+                  <Text>Used Memory: {data.ram.usedMemory}</Text>
+                </>
+              ) : loading ? (
+                <Spinner />
+              ) : (
+                <Text>No RAM data available</Text>
+              )}
+            </VStack>
+          </TabPanel>
+          <TabPanel>
+            <VStack align="center" minH="100px">
+              {data.docker.length > 0 ? (
+                data.docker.map((container) => (
+                  <Box key={container.id} p={1} borderWidth={1} borderRadius="md" width="100%">
+                    <Text fontWeight="bold">Name: {container.name}</Text>
+                    <Text>Status: {container.status}</Text>
+                  </Box>
+                ))
+              ) : loading ? (
+                <Spinner />
+              ) : (
+                <Text>No Docker containers running</Text>
+              )}
+            </VStack>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
+};
+
+export default Popup;
---
+++ frontend/src/pages/Popup/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "'Fira Code', monospace",
+    body: "'Fira Code', monospace",
+  },
+});
+
+export default theme;
