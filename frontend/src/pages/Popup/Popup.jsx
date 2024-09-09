fa9dacb673fd3297c02db0ebf27090bf2a906e01
ansonhex <ansonhe1997@gmail.com>
2024-09-08 23:11:59 -0700

    update UI & added docker details

--- frontend/src/pages/Popup/Popup.jsx
+++ frontend/src/pages/Popup/Popup.jsx
@@ -12,26 +12,23 @@ import {
   CircularProgress,
   CircularProgressLabel,
   VStack,
+  Switch,
+  HStack,
   useColorModeValue,
 } from '@chakra-ui/react';
 
 const getColorScheme = (percentage) => {
-  if (percentage < 50) return "green";
-  if (percentage < 70) return "yellow";
-  return "red";
-};
-
-const getTemperatureColor = (temperature) => {
-  if (temperature < 70) return "green";
-  if (temperature < 80) return "yellow";
-  return "red";
+  if (percentage < 50) return 'green';
+  if (percentage < 70) return 'yellow';
+  return 'red';
 };
 
 const Popup = () => {
   const [data, setData] = useState({ cpu: null, ram: null, docker: [] });
   const [loading, setLoading] = useState(false);
-  const bgColor = useColorModeValue("gray.100", "gray.800");
-  const textColor = useColorModeValue("gray.800", "white");
+  const [detailedView, setDetailedView] = useState(false); // State to manage toggle
+  const bgColor = useColorModeValue('gray.100', 'gray.800');
+  const textColor = useColorModeValue('gray.800', 'white');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,7 +37,11 @@ const Popup = () => {
         const [cpuResponse, ramResponse, dockerResponse] = await Promise.all([
           fetch(`${process.env.REACT_APP_API_URL}/status/cpu`),
           fetch(`${process.env.REACT_APP_API_URL}/status/ram`),
-          fetch(`${process.env.REACT_APP_API_URL}/status/docker`),
+          fetch(
+            `${process.env.REACT_APP_API_URL}/status/docker${
+              detailedView ? '/details' : ''
+            }`
+          ),
         ]);
 
         const [cpu, ram, docker] = await Promise.all([
@@ -58,19 +59,29 @@ const Popup = () => {
     };
 
     fetchData();
-  }, []); 
+  }, [detailedView]); // Fetch data again if detailedView changes
 
   return (
     <Box bg={bgColor} color={textColor} minH="80vh" p={2}>
-      <Heading size="sm" mb={2} textAlign="center">
+      <Heading size="sm" fontWeight="extrabold" mb={2} textAlign="center">
         NAS Status Monitor
       </Heading>
 
       <Tabs isFitted variant="soft-rounded" colorScheme="purple">
         <TabList>
-          <Tab fontSize={"xs"} >CPU</Tab>
-          <Tab fontSize={"xs"} >RAM</Tab>
-          <Tab fontSize={"xs"} >Docker</Tab>
+          <Tab fontSize={'xs'}>CPU</Tab>
+          <Tab fontSize={'xs'}>RAM</Tab>
+          <Tab fontSize={'xs'}>
+            Docker{' '}
+            <Switch
+              size="sm"
+              ml={1}
+              colorScheme="purple"
+              isChecked={detailedView}
+              onChange={() => setDetailedView(!detailedView)}
+            />
+            {/* Switch for toggle */}
+          </Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
@@ -79,20 +90,23 @@ const Popup = () => {
                 <>
                   <CircularProgress
                     value={data.cpu.averageUsagePercentage}
-                    color={`${getColorScheme(data.cpu.averageUsagePercentage)}.400`}
+                    color={`${getColorScheme(
+                      data.cpu.averageUsagePercentage
+                    )}.400`}
                     size="60px"
                     thickness="12px"
                     capIsRound
                   >
-                    <CircularProgressLabel fontSize="xs" fontWeight={"bold"}>
+                    <CircularProgressLabel fontSize="xs" fontWeight={'bold'}>
                       {data.cpu.averageUsagePercentage.toFixed(2)}%
                     </CircularProgressLabel>
                   </CircularProgress>
                   <Text fontWeight="bold" textAlign="center">
                     {data.cpu.model}
                   </Text>
-                  {/* <Text>Cores: {data.cpu.cores}</Text> */}
-                  <Text>Temperature: {data.cpu.temperature || "N/A"}°C</Text>
+                  <Text color={'grey'}>
+                    Temperature: {data.cpu.temperature || 'N/A'}°C
+                  </Text>
                 </>
               ) : loading ? (
                 <Spinner />
@@ -112,13 +126,13 @@ const Popup = () => {
                     thickness="12px"
                     capIsRound
                   >
-                    <CircularProgressLabel fontSize="xs" fontWeight={"bold"}>
+                    <CircularProgressLabel fontSize="xs" fontWeight={'bold'}>
                       {data.ram.usagePercentage.toFixed(2)}%
                     </CircularProgressLabel>
                   </CircularProgress>
-                  <Text>Total: {data.ram.totalMemory}</Text>
-                  <Text>Free: {data.ram.freeMemory}</Text>
-                  <Text>Used: {data.ram.usedMemory}</Text>
+                  <Text color={'grey'}>Total: {data.ram.totalMemory}</Text>
+                  <Text color={'grey'}>Free: {data.ram.freeMemory}</Text>
+                  <Text color={'grey'}>Used: {data.ram.usedMemory}</Text>
                 </>
               ) : loading ? (
                 <Spinner />
@@ -131,9 +145,27 @@ const Popup = () => {
             <VStack align="center" minH="100px">
               {data.docker.length > 0 ? (
                 data.docker.map((container) => (
-                  <Box key={container.id} p={1} borderWidth={1} borderRadius="md" width="100%">
+                  <Box
+                    key={container.id}
+                    p={1}
+                    borderWidth={1}
+                    borderRadius="md"
+                    width="100%"
+                  >
                     <Text fontWeight="bold">{container.name}</Text>
-                    <Text fontSize={"xs"}>{container.status}</Text>
+                    <Text fontSize={'11px'} color={'grey'}>
+                      {container.status}
+                    </Text>
+                    {detailedView && (
+                      <>
+                        <Text fontSize={'11px'} color={'grey'}>
+                          ‣CPU: {container.cpuUsage}
+                        </Text>
+                        <Text fontSize={'11px'} color={'grey'}>
+                          ‣RAM: {container.memoryUsage}
+                        </Text>
+                      </>
+                    )}
                   </Box>
                 ))
               ) : loading ? (
--- frontend/src/pages/Popup/theme.js
+++ frontend/src/pages/Popup/theme.js
@@ -2,8 +2,8 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
-    heading: "'Fira Code', monospace",
-    body: "'Fira Code', monospace",
+    heading: "'JetBrains Mono', monospace",
+    body: "'JetBrains Mono', monospace",
   },
 });
 
